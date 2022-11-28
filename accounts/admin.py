78826acb41919c3eca847ea3ca88ea402426826a
King <king@kali.King>
2022-11-28 16:09:05 +0330

    user models created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'phone', 'is_phone_active', 'is_email_active')
+    list_filter = ()
+
+    fieldsets = (
+        ('Main', {'fields':('email', 'phone', 'first_name', 'last_name', 'password')}),
+        ('permissions', {'fields':('is_active', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields':('username', 'email', 'phone', 'first_name', 'last_name', 'password1', 'password2')}),
+    )
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=11, unique=True, blank = True, null = True)
+    is_phone_active = models.BooleanField(default=False)
+    is_email_active = models.BooleanField(default=True)
+
+
+
+    def __str__(self):
+        return self.email
